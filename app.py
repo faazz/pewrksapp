4e6bcfb6759335f2e0d56e006e95f811653c5248
faazz <farazomar@gmail.com>
2019-02-06 09:16:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import plotly.graph_objs as go
 from flask import Flask
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 server = app.server
 
